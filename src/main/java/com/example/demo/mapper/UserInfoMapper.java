1bbd9f1856079f0ff9e49c758040f122da9a2e87
zhouhuahu <40655443+3537772130@users.noreply.github.com>
2019-06-21 18:34:34 +0800

    上传工具

--- src/main/java/com/example/demo/mapper/UserInfoMapper.java
+++ src/main/java/com/example/demo/mapper/UserInfoMapper.java
@@ -3,8 +3,11 @@ package com.example.demo.mapper;
 import com.example.demo.entity.UserInfo;
 import com.example.demo.entity.UserInfoExample;
 import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+@Mapper
 public interface UserInfoMapper {
     long countByExample(UserInfoExample example);
 
---
+++ src/main/java/com/example/demo/util/IpUtil.java
@@ -0,0 +1,60 @@
+package com.example.demo.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * @program: SpringBootDemo
+ * @description: IP工具类
+ * @author: Mr.ZhouHuaHu
+ * @create: 2019-06-21 11:17
+ **/
+public class IpUtil {
+    private static final Logger log = LoggerFactory.getLogger(IpUtil.class);
+
+    public static String getLocalIp(){
+        try {
+            InetAddress ia = null;
+            ia = ia.getLocalHost();
+
+            return ia.getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String getForIp(HttpServletRequest request){
+        String ipAddress = request.getHeader("x-forwarded-for");
+        if(ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("Proxy-Client-IP");
+        }
+        if(ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if(ipAddress == null || ipAddress.length() == 0 || "unknown".equalsIgnoreCase(ipAddress)) {
+            ipAddress = request.getRemoteAddr();
+            if(ipAddress.equals("127.0.0.1") || ipAddress.equals("0:0:0:0:0:0:0:1")){
+                //根据网卡取本机配置的IP
+                InetAddress inet=null;
+                try {
+                    inet = InetAddress.getLocalHost();
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                }
+                ipAddress= inet.getHostAddress();
+            }
+        }
+        //对于通过多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割
+        if(ipAddress!=null && ipAddress.length()>15){ //"***.***.***.***".length() = 15
+            if(ipAddress.indexOf(",")>0){
+                ipAddress = ipAddress.substring(0,ipAddress.indexOf(","));
+            }
+        }
+        return ipAddress;
+    }
+}
